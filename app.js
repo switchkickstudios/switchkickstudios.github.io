0344bd7578ee7a9781e2bca4e9bbb13fddf7fd71
Conor King <conorking116@gmail.com>
2019-05-28 22:53:33 -0400

    format file structure for heroku deploy

--- app.js
+++ app.js
@@ -4,9 +4,18 @@ const path = require('path');
 const router = express.Router();
 
 router.get('/',function(req,res) {
-    res.render(path.join(__dirname+'/index'))
+    res.render(path.join(__dirname+'/partials/index'))
+});
+router.get("/magicdeckmate", (req, res) => {
+    res.render(path.join(__dirname+'/partials/magicdeckmate'));
+});
+router.get("/kickticker", (req, res) => {
+    res.render(path.join(__dirname+'/partials/kickticker'));
 });
 
+app.use('/dist',express.static(path.join(__dirname+'/dist')));
+app.use('/images',express.static(path.join(__dirname+'/images')));
+app.use('/fonts',express.static(path.join(__dirname+'/fonts')));
 app.set('view engine', 'ejs');
 
 app.use('/', router);
